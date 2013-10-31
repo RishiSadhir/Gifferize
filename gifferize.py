62a3fb538bffe5900f629fd01ee8677f413b2dd2
Rishi Sadhir <Rishi.Sadhir@gmail.com>
2013-10-31 17:07:40 -0400

    Bootstrapped the main page, added an about

--- gifferize.py
+++ gifferize.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -10,12 +11,17 @@ def welcome():
 def display():
     if request.method == 'POST':
         x = ""
-        x += "link : " + request.form['link'] + " "
+        x += "link : " + request.form['link'] + " "  
         x += "start : " + request.form['start'] + " "
         x += "end : " + request.form['end']
         return x
     else:
         return "error!"
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
