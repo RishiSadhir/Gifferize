343322c76f70e1f97eefe21240086b51b2747f99
Rishi Sadhir <Rishi.Sadhir@gmail.com>
2013-11-01 00:51:33 -0400

    Started adding the main logic

--- gifferize.py
+++ gifferize.py
@@ -23,14 +23,65 @@ YT_PASS = config.get("YouTube", "password")
 # Regex for youtube videos
 yt_reg = re.compile("^(https://)?www.youtube.com/.*|(https://)?www.youtu.be/.*|(https://)m.youtube.com/*")
 
+# Render the homepaage
+@app.route('/')
+def welcome():
+    return render_template("welcome.html")
 
-# Random string generator
-def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for x in range(size))
+# Render the about page
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
-# Checks if YouTube link is valid
-def is_valid_yt_link(link):
-    yt_reg
+# upload that gif
+@app.route('/display', methods=['GET', 'POST'])
+def display():
+    # Gather post data
+    if request.method == 'POST':
+        link = request.form['link']
+        start = request.form['start']
+        end = request.form['end']
+
+        if not yt_reg.match(link):
+            return render_template("error.html", error="link",
+                                   message="Looks like the link you gave me wasnt valid :(")
+        if not verify_times(start, end):
+            return render_template("error.html", error="time",
+                                   message="I can't make gifs that long right now, try making it less than 15 seconds.")
+        return youtube_to_gif(link, start, end)
+
+    else:
+        return render_template("error.html")
+
+
+def youtube_to_gif(link, start, end):
+    start, end, diffs = gather_times(start.strip(), end.strip())
+    return "link: "+link+"\nstarts: " + start +"\nends: " + end
+
+def verify_times(start, end):
+    starts = [int(x.strip()) for x in start.split(":")[::-1]]
+    ends = [int(x.strip()) for x in end.split(":")[::-1]]
+
+    diff = []
+
+    for i in range(0, len(starts)):
+        diff.append(ends[i] - starts[i])
+
+        if diff[i] < 0:
+            ends[i+1] -= 1
+            diff[i] += 60
+
+        if diff[0] > 15:
+            return False
+
+        for i in range(1, len(diff)):
+            if diff[i] > 0:
+                return False
+    return True
+    
+
+def gather_times(start, end):
+    return start, end, end 
 
 # Uploads gif to imgur
 def imgur_upload(path):
@@ -46,30 +97,10 @@ def imgur_upload(path):
         })
     return rqst.json()["data"]["link"]
 
-# Render the homepaage
-@app.route('/')
-def welcome():
-    return render_template("welcome.html")
 
-# Render the about page
-@app.route('/about')
-def about():
-    return render_template("about.html")
-
-# upload that gif!
-@app.route('/display', methods=['GET', 'POST'])
-def display():
-    if request.method == 'POST':
-        link = request.form['link']
-        start = request.form['start']
-        end = request.form['end']
-
-        return link + " " + start + " " + end
-    else:
-        return "error in post method!"
-
-
+# Random string generator
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for x in range(size))
 
 if __name__ == '__main__':
     app.run(debug=True)
-
